e4e0d5515964a666e318937b72c47986840acd86
zapicojunredexter <zapicojunredexter@yahoo.com>
2018-08-06 15:17:09 +0800

    make collider around cameramera

---
+++ Assets/Scripts/Bouncer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncer : MonoBehaviour {
+
+	Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		Debug.Log("hitted" + col.gameObject.name);
+		if(col.gameObject.name == "Spring") {
+
+		} else {
+		rb2d.AddForce(Vector3.up * 500);
+		}
+	}
+}
